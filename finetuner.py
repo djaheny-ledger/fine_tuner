5d38a3de5b8530c4eb27d357d771a841bbbc29d1
Dan JAHENY <danieljaheny@gmail.com>
2023-08-26 16:51:47 +0200

    finetuner

--- finetuner.py
+++ finetuner.py
@@ -2,9 +2,11 @@ import os
 import openai
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
-openai.FineTuningJob.create(training_file="file-abc123", model="gpt-3.5-turbo")
 
-openai.File.create(
-  file=open("mydata.jsonl", "rb"),
-  purpose='fine-tune'
-)
+training  = openai.File.create(
+                    file=open("primed_data.jsonl", "rb"),
+                    purpose='fine-tune'
+                    )
+
+openai.FineTuningJob.create(training_file=training, model="gpt-3.5-turbo")
+
